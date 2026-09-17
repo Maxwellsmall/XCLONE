8dbd3ba34c757bb2a78f772290340a57e18dac6d
Maxwellsmall <maxwellsmall88@gmail.com>
2026-09-17 20:19:12 +0100

    have added the switching tab on the follow page

--- xclone-frontend/app/(routes)/follow/page.tsx
+++ xclone-frontend/app/(routes)/follow/page.tsx
@@ -17,13 +17,15 @@ const page = () => {
         <Settings/>
       </div>
       <div className='flex justify-between items-center mx-3 my-4'>
-        <div className='flex justify-center items-center w-[50%] my-3'>
+        <div className='flex justify-center items-center w-[50%] my-3' onClick={() => setIsActive(true)}>
 
-        <h1 className="text-gray-400 cursor-pointer">Who to follow</h1>
+        <h1 className={`text-gray-400 cursor-pointer 
+          ${isActive === true ? "border-b-4 border-blue-500 pb-3" : "border-0"}
+          `}>Who to follow</h1>
         </div>
         <div className='flex justify-center items-center w-[50%] my-3'>
 
-        <h1 className="text-gray-400 cursor-pointer border-b-4 border-blue-500 pb-3">Creators to follow</h1>
+        <h1 className={`text-gray-400 cursor-pointer ${!isActive === false ? "border-b-4 border-blue-500 pb-3" : "border-0"}`}>Creators to follow</h1>
         </div>
       </div>
       <div className="border-t-2 border-gray-900 w-full"/>
