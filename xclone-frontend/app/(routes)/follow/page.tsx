213daabbdb999cc18dc44b08f16627d9f0249495
Maxwellsmall <maxwellsmall88@gmail.com>
2026-09-18 09:57:48 +0100

    have done the follow props componenent

--- xclone-frontend/app/(routes)/follow/page.tsx
+++ xclone-frontend/app/(routes)/follow/page.tsx
@@ -3,9 +3,36 @@
 import { ArrowLeft, Settings } from 'lucide-react'
 import React from 'react'
 import { useState } from 'react'
+import Follow from "@/app/components/followprops"
+
+
 
 const page = () => {
   const [isActive, setIsActive] = useState(false)
+
+  const followdata = [
+    {
+      name: "Maxwell Edunfunke",
+      username: "@web3Threat",
+      description: "i am a fukkstack web developer and also a web3 enthusiast"
+  },
+  {
+      name: "Maxwell Edunfunke",
+      username: "@web3Threat",
+      description: "i am a fukkstack web developer and also a web3 enthusiast"
+  },
+  {
+      name: "Maxwell Edunfunke",
+      username: "@web3Threat",
+      description: "i am a fukkstack web developer and also a web3 enthusiast"
+  },
+
+  {
+      name: "Maxwell Edunfunke",
+      username: "@web3Threat",
+      description: "i am a fukkstack web developer and also a web3 enthusiast"
+  },
+  ]
 
   return (
     <div className='w-full flex flex-col'>
@@ -16,19 +43,38 @@ const page = () => {
         </div>
         <Settings/>
       </div>
-      <div className='flex justify-between items-center mx-3 my-4'>
-        <div className='flex justify-center items-center w-[50%] my-3' onClick={() => setIsActive(true)}>
+      <div className='flex justify-between items-center mx-3 my-2'>
+        <div className='flex justify-center items-center w-[50%] my-3' onClick={() => setIsActive(false)}>
 
         <h1 className={`text-gray-400 cursor-pointer 
-          ${isActive === true ? "border-b-4 border-blue-500 pb-3" : "border-0"}
+          ${!isActive ? "border-b-4 border-blue-500 pb-3" : "border-0"}
           `}>Who to follow</h1>
         </div>
-        <div className='flex justify-center items-center w-[50%] my-3'>
+        <div className='flex justify-center items-center w-[50%] my-3' onClick={() => setIsActive(true)}>
 
-        <h1 className={`text-gray-400 cursor-pointer ${!isActive === false ? "border-b-4 border-blue-500 pb-3" : "border-0"}`}>Creators to follow</h1>
+        <h1 className={`text-gray-400 cursor-pointer ${isActive ? "border-b-4 border-blue-500 pb-3" : "border-0"}`}>Creators to follow</h1>
         </div>
       </div>
       <div className="border-t-2 border-gray-900 w-full"/>
+      <div className="flex w-full items-start flex-col">
+          <div className='flex w-full items-start flex-col mx-5 py-2'>
+              <h1 className="font-semibold text-[25px] text-white">Suggested for you</h1>
+                <div className='w-full flxx'>
+                  {followdata.map((items) => (
+                      <div>
+                        <Follow
+                        name={items.name}
+                        username={items.username}
+                        image={items.image}
+                        description={items.description}
+                        />
+                      </div>
+                  ))
+
+                  }
+                </div>
+          </div>
+      </div>
     </div>
   )
 }
---
+++ xclone-frontend/app/components/followprops.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+interface Props {
+    image: string
+    name: string
+    username: string
+    description: string
+}
+
+const Followprops: React.FC<Props> = ({image, name, username, description}) => {
+
+  return (
+    <>
+    <div className='flex flex-col w-full items-center justify-center'>
+        <div className='flex flex-col w-full mx-5'>
+            <div>
+                <img src={image} alt="" className='bg-gray-500 rounded-full' />
+            </div>
+            <div className='flex items-start flex-col w-full'>
+                <h1>{name}</h1>
+                <p>{username}</p>
+                <p>{description}</p>
+            </div>
+            <div className='flex justify-center justify-center'>
+            <button className='py-2 px-3 text-black bg-white rounded-full'>Follow</button>
+
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Followprops
