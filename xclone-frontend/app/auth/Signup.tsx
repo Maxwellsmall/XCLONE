f4a56a079f46099b3726efe00ed97e7d65f742ce
Maxwellsmall <maxwellsmall88@gmail.com>
2026-09-21 13:25:55 +0100

    am working on the signup page

--- xclone-frontend/app/auth/Signup.tsx
+++ xclone-frontend/app/auth/Signup.tsx
@@ -1,15 +1,22 @@
 // app/page.tsx
+// import Image from "@/public/vercel.svg"
 
+import { X } from "lucide-react";
 import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center bg-black text-white">
-     <Link href="/home">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 cursor-pointer rounded">
-        Sign Up
-      </button>
-     </Link>
+    <main className="flex min-h-screen items-center justify-center bg-black text-white w-full">
+     <div className="flex justify-between items-center w-full h-screen">
+        <div className="flex flex-col items-center justify-start w-[50%]">
+          <h1 className="font-bold text-[50px] max-w-[300px]">Happening <span className="">now.</span>  </h1>
+        </div>
+
+        
+        <div className="flex flex-col items-center justify-center w-[50%]">
+          <X className="text-white w-[500px] h-[500px]"/>
+        </div>
+     </div>
     </main>
   );
 }
